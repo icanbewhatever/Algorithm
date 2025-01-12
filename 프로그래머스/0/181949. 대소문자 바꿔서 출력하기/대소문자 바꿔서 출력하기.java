14c722ab63da0a3074e9c7574c84060f5fdf4815
Hyunju Lee (Luna) <139785614+icanbewhatever@users.noreply.github.com>
2025-01-13 05:45:15 +0900

    [level 0] Title: 대소문자 바꿔서 출력하기, Time: 165.00 ms, Memory: 67.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/181949. 대소문자 바꿔서 출력하기/대소문자 바꿔서 출력하기.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        
+        StringBuilder sb = new StringBuilder();
+        
+        for(int i =0; i< str.length(); i++){
+            
+            char c = str.charAt(i);
+             
+            if (Character.isUpperCase(c)){
+                sb.append(Character.toLowerCase(c));
+            } else if (Character.isLowerCase(c)){
+                sb.append(Character.toUpperCase(c));
+            } else{
+                sb.append(c);
+            }
+            
+        }
+            System.out.println(sb);
+    }
+}
